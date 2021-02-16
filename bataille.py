ab17b21bea32d25b3aa553df939f0a946682a206
gw8ndal <61947142+gw8ndal@users.noreply.github.com>
2021-02-16 09:39:54 +0100

    Update bataille.py

--- bataille.py
+++ bataille.py
@@ -1,57 +1,68 @@
 import random
 import time
+import os
 
-nombres = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi', 'As')
-
-couleurs = ('Carreau', 'Coeur', 'Pique', 'Trêfle')
+VALEURS = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'VALET', 'DAME', 'ROI', 'AS')
+COULEURS = ('COEUR', 'CARREAU', 'TREFLE', 'PIQUE')
 
 jeu = []
 
-joueur1 = []
+jeu1 = []
 
-joueur2 = []
+jeu2 = []
 
+table = []
 
-# fonction pour assigner une couleur à chaque carte
 def creer_jeu():
-    
-    for n in nombres:
-        for c in couleurs:
+    for n in VALEURS:
+        for c in COULEURS:
             carte = (n, c)
             jeu.append(carte)
 
     return(jeu)
 
-# fonction pour mélanger le jeu
-def melanger_jeu():
+def melanger(jeu):
     random.shuffle(jeu)
     return(jeu)
 
-# fonction pour distribuer les cartes
-def distribuer_jeu():
-    joueur1 = jeu[0:len(jeu)//2]
-    joueur2 = jeu[len(jeu)//2:-1]
-    return(joueur1, joueur2)
+def distribuer(jeu):
+    jeu1 = jeu[0:len(jeu)//2]
+    jeu2 = jeu[len(jeu)//2:-1]
+    return(jeu1, jeu2)
+
+def jouer(jeu1, jeu2):
+    while len(jeu1) != 0 or len(jeu2) != 0:
+
+        carte1 = jeu1[0]
+        jeu1 = jeu1[1:-1]
+        carte2 = jeu2[0]
+        jeu2 = jeu2[1:-1]
+        print('Joueur 1 : ', carte1, 'Joueur 2 : ', carte2)
+
+        table.append(carte1)
+        table.append(carte2)
+        if VALEURS.index(carte1[0]) > VALEURS.index(carte2[0]):
+            jeu1.extend(table)
+            jeu2.pop(0)
+        if VALEURS.index(carte2[0]) > VALEURS.index(carte1[0]):
+            jeu2.extend(table)
+            jeu1.pop(0)
+        print('table : ',table)
+        print('jeu1 : ',jeu1)
+        print('jeu2 : ',jeu2)
+        os.system('pause')
 
 
 
+def demarrer_partie():
+    """
+    Créer un jeu de carte, mélange les cartes, les distribue en 2 paquets et lance le jeu.
+    lancer un jeu
+    retourne le tuple jeu1, jeu2
+    """
+    jeu = creer_jeu()
+    jeu = melanger(jeu)
+    jeu1, jeu2 = distribuer(jeu)
+    return jouer(jeu1, jeu2)
 
-creer_jeu()
-distribuer_jeu()
-print('Joueur 1 : ', joueur1)
-print('Joueur 2 : ', joueur2)
-
-def jouer():
-    for tour in range(len(jeu)//2):
-        for carte1 in joueur1:
-            for carte2 in joueur2:
-                print('Joueur 1 : ', carte1, 'Joueur 2 : ', carte2)
-                time.sleep(2)
- 
-
-jouer()
-
-
-#    reste a faire : 
-# - fonction pour montrer les cartes une par une
-# - redonner la carte a celui qui a joué la plus haute
+print(demarrer_partie())
